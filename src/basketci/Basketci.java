8b0614c08176c709f2d52569669a2e18fcc48975
Zeki ÇIPLAK <zekiciplak@gmail.com>
2022-01-16 14:43:33 +0300

    Update Basketci.java

--- src/basketci/Basketci.java
+++ src/basketci/Basketci.java
@@ -1,25 +1,7 @@
-//---------------------------------------------
+//-------------------
 //  Zeki ÇIPLAK 
-//  523621009
-//---------------------------------
-//  BASKETÇİ OYUNU NASIL OYNANIR?
-//
-//  Oyun Öncesi
-//  Oyun başlamadan önce skor tablosuna eklenmesi amacıyla oyuncu ismi girmeniz beklenir.
-//  Oyun başladıktan sonra, klavyedeki herhangi bir tuşla topu fırlatabilirsiniz. (Space tuşu önerilir)
-//
-//  Oyun Süresi
-//  Oyunun belirlenmiş bir süresi vardır. (Test için 90 saniye olarak ayarlandı)
-//  Belirlenen sürede maksimum basket yaparak, puanın arttırılması hedeflenir.
-//
-//  Puanlama
-//  Her basket için 10 puan kazanılır.
-//  Basket olmayan her atış için 1 puan kaybedilir.
-//
-//  Oyun Sonu
-//  Oyun sırasında başlıkta, oyun sonunda ise skor tablosunda puan gösterilir.
-//  Oyuncu puan sıralamasında ilk 10'a girdiyse listede adını görebilir.
-//---------------------------------------------
+//  gitub.com/zkcplk
+//-------------------
 package basketci;
 
 import java.io.File;
